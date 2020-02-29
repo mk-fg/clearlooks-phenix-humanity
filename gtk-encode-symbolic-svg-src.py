773055762dfed80680a1a34004c74254c0ad2319
Mike Kazantsev <mk.fraggod@gmail.com>
2020-03-01 02:21:55 +0500

    text-input.combobox: use custom svg arrow icon

--- gtk-encode-symbolic-svg-src.py
+++ gtk-encode-symbolic-svg-src.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python
 
 import os, sys, pathlib as pl, subprocess as sp
-import xml.etree.ElementTree as et
 
 # glob-patterns for files to process with gtk-encode-symbolic-svg
-svg_globs = ['arrow-*.svg']
+svg_globs = ['dropdown-*.svg']
 p_img = pl.Path(__file__).parent / 'gtk-3.0/img'
 
 for pat in svg_globs:
 	for p in p_img.glob(pat):
-		p_png = p_img / (p.name.rsplit('.', 1)[0] + '.symbolic.png')
+		p_png = p_img / (p.name.rsplit('.', 1)[0] + '.png')
 		try: ts_dst = p_png.stat().st_mtime
 		except OSError: ts_dst = 0
 		if p.stat().st_mtime <= ts_dst: continue
 
-		svg = et.parse(p).getroot()
-		wh = svg.attrib['width'] + 'x' + svg.attrib['height']
-
-		cmd = ['gtk-encode-symbolic-svg', '-o', str(p_img), str(p), wh]
+		# Note: gtk-encode-symbolic-svg produces very blurry icons, unlike inkscape
+		cmd = (
+			'inkscape --without-gui --export-dpi=90 --export-background-opacity=0'.split()
+			+ ['--export-png', str(p_png), str(p)] )
 		print(' '.join(cmd))
-		sp.run(cmd, check=True)
+		sp.run(cmd, check=True, stdout=sp.DEVNULL, stderr=sp.DEVNULL)
